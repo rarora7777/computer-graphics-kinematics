872d2808dcc6be44ae10ac221f9873f5590a66c2
Rahul Arora <arorar@cs.toronto.edu>
2019-11-27 17:34:29 -0500

    Minor

---
+++ replace.sh
@@ -0,0 +1,162 @@
+# Vectors and Matrices
+sed -i 's/\\A\([^A-Za-z]\)/\\mathbf\{A\}\1/g' $1
+sed -i 's/\\B\([^A-Za-z]\)/\\mathbf\{B\}\1/g' $1
+sed -i 's/\\C\([^A-Za-z]\)/\\mathbf\{C\}\1/g' $1
+sed -i 's/\\D\([^A-Za-z]\)/\\mathbf\{D\}\1/g' $1
+sed -i 's/\\E\([^A-Za-z]\)/\\mathbf\{E\}\1/g' $1
+sed -i 's/\\F\([^A-Za-z]\)/\\mathbf\{F\}\1/g' $1
+sed -i 's/\\G\([^A-Za-z]\)/\\mathbf\{G\}\1/g' $1
+sed -i 's/\\H\([^A-Za-z]\)/\\mathbf\{H\}\1/g' $1
+sed -i 's/\\I\([^A-Za-z]\)/\\mathbf\{I\}\1/g' $1
+sed -i 's/\\J\([^A-Za-z]\)/\\mathbf\{J\}\1/g' $1
+sed -i 's/\\K\([^A-Za-z]\)/\\mathbf\{K\}\1/g' $1
+sed -i 's/\\L\([^A-Za-z]\)/\\mathbf\{L\}\1/g' $1
+sed -i 's/\\M\([^A-Za-z]\)/\\mathbf\{M\}\1/g' $1
+sed -i 's/\\N\([^A-Za-z]\)/\\mathbf\{N\}\1/g' $1
+sed -i 's/\\One\([^A-Za-z]\)/\\mathbf\{1\}\1/g' $1
+sed -i 's/\\P\([^A-Za-z]\)/\\mathbf\{P\}\1/g' $1
+sed -i 's/\\Q\([^A-Za-z]\)/\\mathbf\{Q\}\1/g' $1
+sed -i 's/\\R\([^A-Za-z]\)/\\mathbf\{R\}\1/g' $1
+sed -i 's/\\S\([^A-Za-z]\)/\\mathbf\{S\}\1/g' $1
+sed -i 's/\\T\([^A-Za-z]\)/\\mathbf\{T\}\1/g' $1
+sed -i 's/\\U\([^A-Za-z]\)/\\mathbf\{U\}\1/g' $1
+sed -i 's/\\V\([^A-Za-z]\)/\\mathbf\{V\}\1/g' $1
+sed -i 's/\\W\([^A-Za-z]\)/\\mathbf\{W\}\1/g' $1
+sed -i 's/\\X\([^A-Za-z]\)/\\mathbf\{X\}\1/g' $1
+sed -i 's/\\Y\([^A-Za-z]\)/\\mathbf\{Y\}\1/g' $1
+sed -i 's/\\a\([^A-Za-z]\)/\\mathbf\{a\}\1/g' $1
+sed -i 's/\\b\([^A-Za-z]\)/\\mathbf\{b\}\1/g' $1
+sed -i 's/\\c\([^A-Za-z]\)/\\mathbf\{c\}\1/g' $1
+sed -i 's/\\d\([^A-Za-z]\)/\\mathbf\{d\}\1/g' $1
+sed -i 's/\\e\([^A-Za-z]\)/\\mathbf\{e\}\1/g' $1
+sed -i 's/\\f\([^A-Za-z]\)/\\mathbf\{f\}\1/g' $1
+sed -i 's/\\g\([^A-Za-z]\)/\\mathbf\{g\}\1/g' $1
+sed -i 's/\\h\([^A-Za-z]\)/\\mathbf\{h\}\1/g' $1
+sed -i 's/\\i\([^A-Za-z]\)/\\mathbf\{i\}\1/g' $1
+sed -i 's/\\j\([^A-Za-z]\)/\\mathbf\{j\}\1/g' $1
+sed -i 's/\\k\([^A-Za-z]\)/\\mathbf\{k\}\1/g' $1
+sed -i 's/\\l\([^A-Za-z]\)/\\mathbf\{l\}\1/g' $1
+sed -i 's/\\m\([^A-Za-z]\)/\\mathbf\{m\}\1/g' $1
+sed -i 's/\\n\([^A-Za-z]\)/\\mathbf\{n\}\1/g' $1
+sed -i 's/\\p\([^A-Za-z]\)/\\mathbf\{p\}\1/g' $1
+sed -i 's/\\q\([^A-Za-z]\)/\\mathbf\{q\}\1/g' $1
+sed -i 's/\\r\([^A-Za-z]\)/\\mathbf\{r\}\1/g' $1
+sed -i 's/\\s\([^A-Za-z]\)/\\mathbf\{s\}\1/g' $1
+sed -i 's/\\t\([^A-Za-z]\)/\\mathbf\{t\}\1/g' $1
+sed -i 's/\\u\([^A-Za-z]\)/\\mathbf\{u\}\1/g' $1
+sed -i 's/\\v\([^A-Za-z]\)/\\mathbf\{v\}\1/g' $1
+sed -i 's/\\x\([^A-Za-z]\)/\\mathbf\{x\}\1/g' $1
+sed -i 's/\\y\([^A-Za-z]\)/\\mathbf\{y\}\1/g' $1
+sed -i 's/\\z\([^A-Za-z]\)/\\mathbf\{z\}\1/g' $1
+sed -i 's/\\0\([^A-Za-z]\)/\\mathbf\{0\}\1/g' $1
+
+sed -i 's/\\A$/\\mathbf\{A\}/g' $1
+sed -i 's/\\B$/\\mathbf\{B\}/g' $1
+sed -i 's/\\C$/\\mathbf\{C\}/g' $1
+sed -i 's/\\D$/\\mathbf\{D\}/g' $1
+sed -i 's/\\E$/\\mathbf\{E\}/g' $1
+sed -i 's/\\F$/\\mathbf\{F\}/g' $1
+sed -i 's/\\G$/\\mathbf\{G\}/g' $1
+sed -i 's/\\H$/\\mathbf\{H\}/g' $1
+sed -i 's/\\I$/\\mathbf\{I\}/g' $1
+sed -i 's/\\J$/\\mathbf\{J\}/g' $1
+sed -i 's/\\K$/\\mathbf\{K\}/g' $1
+sed -i 's/\\L$/\\mathbf\{L\}/g' $1
+sed -i 's/\\M$/\\mathbf\{M\}/g' $1
+sed -i 's/\\N$/\\mathbf\{N\}/g' $1
+sed -i 's/\\One$/\\mathbf\{1\}/g' $1
+sed -i 's/\\P$/\\mathbf\{P\}/g' $1
+sed -i 's/\\Q$/\\mathbf\{Q\}/g' $1
+sed -i 's/\\R$/\\mathbb\{R\}/g' $1
+sed -i 's/\\S$/\\mathbf\{S\}/g' $1
+sed -i 's/\\T$/\\mathbf\{T\}/g' $1
+sed -i 's/\\U$/\\mathbf\{U\}/g' $1
+sed -i 's/\\V$/\\mathbf\{V\}/g' $1
+sed -i 's/\\W$/\\mathbf\{W\}/g' $1
+sed -i 's/\\X$/\\mathbf\{X\}/g' $1
+sed -i 's/\\Y$/\\mathbf\{Y\}/g' $1
+sed -i 's/\\a$/\\mathbf\{a\}/g' $1
+sed -i 's/\\b$/\\mathbf\{b\}/g' $1
+sed -i 's/\\c$/\\mathbf\{c\}/g' $1
+sed -i 's/\\d$/\\mathbf\{d\}/g' $1
+sed -i 's/\\e$/\\mathbf\{e\}/g' $1
+sed -i 's/\\f$/\\mathbf\{f\}/g' $1
+sed -i 's/\\g$/\\mathbf\{g\}/g' $1
+sed -i 's/\\h$/\\mathbf\{h\}/g' $1
+sed -i 's/\\i$/\\mathbf\{i\}/g' $1
+sed -i 's/\\j$/\\mathbf\{j\}/g' $1
+sed -i 's/\\k$/\\mathbf\{k\}/g' $1
+sed -i 's/\\l$/\\mathbf\{l\}/g' $1
+sed -i 's/\\m$/\\mathbf\{m\}/g' $1
+sed -i 's/\\n$/\\mathbf\{n\}/g' $1
+sed -i 's/\\p$/\\mathbf\{p\}/g' $1
+sed -i 's/\\q$/\\mathbf\{q\}/g' $1
+sed -i 's/\\r$/\\mathbf\{r\}/g' $1
+sed -i 's/\\s$/\\mathbf\{s\}/g' $1
+sed -i 's/\\t$/\\mathbf\{t\}/g' $1
+sed -i 's/\\u$/\\mathbf\{u\}/g' $1
+sed -i 's/\\v$/\\mathbf\{v\}/g' $1
+sed -i 's/\\x$/\\mathbf\{x\}/g' $1
+sed -i 's/\\y$/\\mathbf\{y\}/g' $1
+sed -i 's/\\z$/\\mathbf\{z\}/g' $1
+sed -i 's/\\0$/\\mathbf\{0\}/g' $1
+
+#Operators
+sed -i 's/\\vec\([^A-Za-z]\)/\\mathbf\1/g' $1
+sed -i 's/\\mat\([^A-Za-z]\)/\\mathbf\1/g' $1
+sed -i 's/\\Rot\([^A-Za-z]\)/\\mathbf\{R\}\1/g' $1
+sed -i 's/\\min\([^A-Za-z]\)/\\mathop\{\\text\{min\}\}\1/g' $1
+sed -i 's/\\argmax\([^A-Za-z]\)/\\mathop\{\\text\{argmax\}\}\1/g' $1
+sed -i 's/\\argmin\([^A-Za-z]\)/\\mathop\{\\text\{argmin\}\}\1/g' $1
+sed -i 's/\\transpose\([^A-Za-z]\)/\{\\mathsf T\}\1/g' $1
+#sed -i 's/\\tr\([^A-Za-z]\)/\\mathop\{\\text\{tr\}\}\1/g' $1
+perl -pi -e 's/\\tr\s*(\{((?:[^{}]++|(?1))*)\})/\\mathop\{\\text\{tr\}\}\2/g' $1
+
+#Special characters
+sed -i 's/∈/\\in /g' $1
+sed -i 's/³/\^3 /g' $1
+sed -i 's/½/\\frac12 /g' $1
+sed -i 's/∂/\\partial /g' $1
+sed -i 's/∆/\\Delta /g' $1
+sed -i 's/²/\^2 /g' $1
+sed -i 's/‖/\\| /g' $1
+sed -i 's/↔/\\Leftrightarrow /g' $1
+sed -i 's/→/\\Rightarrow /g' $1
+sed -i 's/∀/\\forall /g' $1
+sed -i 's/θ/\\theta /g' $1
+sed -i 's/Θ/\\Theta /g' $1
+sed -i 's/₁/\_1 /g' $1
+sed -i 's/₂/\_2 /g' $1
+sed -i 's/₃/\_3 /g' $1
+sed -i 's/φ/\\varphi /g' $1
+sed -i 's/°/\^\\circ /g' $1
+sed -i 's/←/\\Leftarrow /g' $1
+sed -i 's/σ/\\sigma /g' $1
+sed -i 's/⁰/\^0 /g' $1
+sed -i 's/δ/\\delta /g' $1
+sed -i 's/ε/\\epsilon /g' $1
+sed -i 's/≈/\\approx /g' $1
+sed -i 's/∑/\\Sigma /g' $1
+sed -i 's/∞/\\infty /g' $1
+sed -i 's/Σ/\\sigma /g' $1
+sed -i 's/Ω/\\Omega /g' $1
+sed -i 's/π/\\pi /g' $1
+sed -i 's/ψ/\\psi /g' $1
+sed -i 's/≠/\\ne /g' $1
+sed -i 's/∫/\\int /g' $1
+sed -i 's/∆/\\Delta /g' $1
+sed -i 's/∆/\\Delta /g' $1
+sed -i 's/±/\\pm /g' $1
+sed -i 's/≥/\\ge /g' $1
+sed -i 's/≤/\\le /g' $1
+sed -i 's/×/\\times /g' $1
+sed -i 's/…/\\ldots/g' $1
+sed -i 's/…/\\ldots/g' $1
+#sed -i 's///g' $1
+
+# Alec's escaped chars
+sed -i 's/\\\_/\_/g' $1
+sed -i 's/\\\^/\^/g' $1
+
+# Misc.
+sed -i 's/\\hat\([^A-Za-z]\)/\\widehat\1/g' $1
